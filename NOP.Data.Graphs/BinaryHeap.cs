fffc1d0ab3830c22ac3ad9f1f2d874363f1b79c0
Дмитрий Калашников <dmitry.k@hotmail.com>
2021-12-14 16:21:42 +0600

    Add project files.

---
+++ NOP.Data.Graphs/BinaryHeap.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NOP.Data.Graphs
+{
+    public sealed class BinaryHeap<TItem, TPrioriry>
+    {
+        private readonly IEqualityComparer<TItem> _itemComparer;
+
+        private readonly IComparer<TPrioriry> _priorityComparer;
+
+        private readonly List<(TItem Item, TPrioriry Priority)> _heap = new();
+
+        public BinaryHeap(IEqualityComparer<TItem> itemComparer = null, IComparer<TPrioriry> priorityComparer = null)
+        {
+            _itemComparer = itemComparer ?? EqualityComparer<TItem>.Default;
+            _priorityComparer = priorityComparer ?? Comparer<TPrioriry>.Default;
+        }
+
+        public void Enqueue(TItem item, TPrioriry priority)
+        {
+            _heap.Add((item, priority));
+
+            if (_heap.Count > 1)
+            {
+                ShiftUp(_heap.Count - 1);
+            }
+        }
+
+        public bool TryDequeue(out TItem item, out TPrioriry priority)
+        {
+            if (_heap.Count == 0)
+            {
+                item = default;
+                priority = default;
+
+                return false;
+            }
+            else if (_heap.Count == 1)
+            {
+                (item, priority) = _heap[0];
+                _heap.Clear();
+
+                return true;
+            }
+
+            (item, priority) = _heap[0];
+            _heap[0] = _heap[^1];
+            _heap.RemoveAt(_heap.Count - 1);
+
+            ShiftDown(0);
+
+            return true;
+        }
+
+        public bool DecreasePriority(TItem item, TPrioriry priority)
+        {
+            var i = _heap.FindIndex(x => _itemComparer.Equals(x.Item, item) && _priorityComparer.Compare(x.Priority, priority) > 0);
+            if (i == -1)
+            {
+                return false;
+            }
+
+            _heap[i] = (item, priority);
+            ShiftUp(i);
+
+            return true;
+        }
+
+        public void RemoveWhere(Predicate<TItem> match)
+        {
+            if (match == null)
+            {
+                throw new ArgumentNullException(nameof(match));
+            }
+
+            for (int i = _heap.Count - 1; i >= 0; i--)
+            {
+                if (match(_heap[i].Item))
+                {
+                    if (i == _heap.Count - 1)
+                    {
+                        _heap.RemoveAt(_heap.Count - 1);
+                    }
+                    else
+                    {
+                        _heap[i] = _heap[^1];
+                        _heap.RemoveAt(_heap.Count - 1);
+
+                        if (i == 0 || _priorityComparer.Compare(_heap[i / 2].Priority, _heap[i].Priority) < 0)
+                        {
+                            ShiftDown(i);
+                        }
+                        else
+                        {
+                            ShiftUp(i);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ShiftUp(int i)
+        {
+            while (_priorityComparer.Compare(_heap[i].Priority, _heap[(i - 1) / 2].Priority) < 0)
+            {
+                Swap(i, (i - 1) / 2);
+
+                i = (i - 1) / 2;
+            }
+        }
+
+        private void ShiftDown(int i)
+        {
+            while ((2 * i) + 1 < _heap.Count)
+            {
+                var left = (2 * i) + 1;
+                var right = (2 * i) + 2;
+
+                var j = left;
+                if (right < _heap.Count && _priorityComparer.Compare(_heap[right].Priority, _heap[left].Priority) < 0)
+                {
+                    j = right;
+                }
+
+                if (_priorityComparer.Compare(_heap[i].Priority, _heap[j].Priority) <= 0)
+                {
+                    break;
+                }
+
+                Swap(i, j);
+                i = j;
+            }
+        }
+
+        private void Swap(int i, int j)
+        {
+            var temp = _heap[i];
+
+            _heap[i] = _heap[j];
+            _heap[j] = temp;
+        }
+    }
+}
---
+++ NOP.Data.Graphs/DijkstraPathBuilder.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NOP.Data.Graphs
+{
+    public sealed class DijkstraPathBuilder<TVertex, TDistance>
+    {
+        public delegate bool TryGetDistance(TVertex startVertex, TDistance startDistance, TVertex endVertex, out TDistance endDistance);
+
+        private readonly IReadOnlyDictionary<TVertex, IReadOnlyCollection<TVertex>> _graph;
+
+        private readonly TryGetDistance _tryGetDistance;
+
+        private readonly IEqualityComparer<TVertex> _vertexComparer;
+
+        private readonly IComparer<TDistance> _distanceComparer;
+
+        private readonly Dictionary<TVertex, TVertex> _verticesOrder = new();
+
+        private readonly Dictionary<TVertex, TDistance> _verticesDistance = new();
+
+        public IReadOnlyDictionary<TVertex, TVertex> VerticesOrder => _verticesOrder;
+
+        public IReadOnlyDictionary<TVertex, TDistance> VerticesDistance => _verticesDistance;
+
+        public DijkstraPathBuilder(
+            IReadOnlyDictionary<TVertex, IReadOnlyCollection<TVertex>> graph,
+            TryGetDistance tryGetDistance,
+            IEqualityComparer<TVertex> vertexComparer = null,
+            IComparer<TDistance> distanceComparer = null)
+        {
+            _graph = graph ?? throw new ArgumentNullException(nameof(graph));
+            _tryGetDistance = tryGetDistance ?? throw new ArgumentNullException(nameof(tryGetDistance));
+            _vertexComparer = vertexComparer ?? EqualityComparer<TVertex>.Default;
+            _distanceComparer = distanceComparer ?? Comparer<TDistance>.Default;
+        }
+
+        public DijkstraPathBuilder<TVertex, TDistance> Build(TVertex startVertex)
+        {
+            _verticesOrder.Clear();
+            _verticesDistance.Clear();
+
+            var verticesQueue = new BinaryHeap<TVertex, TDistance>(_vertexComparer, _distanceComparer);
+            verticesQueue.Enqueue(startVertex, default);
+
+            while (verticesQueue.TryDequeue(out var nearestVertex, out var nearestVertexDistance))
+            {
+                if (!_graph.TryGetValue(nearestVertex, out var neighborVertices))
+                {
+                    continue;
+                }
+
+                foreach (var neighborVertex in neighborVertices)
+                {
+                    if (_vertexComparer.Equals(neighborVertex, startVertex))
+                    {
+                        continue;
+                    }
+
+                    if (!_tryGetDistance(nearestVertex, nearestVertexDistance, neighborVertex, out var neighborVertexDistance))
+                    {
+                        continue;
+                    }
+
+                    if (!_verticesDistance.TryGetValue(neighborVertex, out var neighborVertexDistancePrev))
+                    {
+                        _verticesDistance[neighborVertex] = neighborVertexDistance;
+                        _verticesOrder[neighborVertex] = nearestVertex;
+                        verticesQueue.Enqueue(neighborVertex, neighborVertexDistance);
+                    }
+                    else if (_distanceComparer.Compare(neighborVertexDistance, neighborVertexDistancePrev) < 0)
+                    {
+                        _verticesDistance[neighborVertex] = neighborVertexDistance;
+                        _verticesOrder[neighborVertex] = nearestVertex;
+                        verticesQueue.DecreasePriority(neighborVertex, neighborVertexDistance);
+                    }
+                }
+            }
+
+            return this;
+        }
+    }
+}
